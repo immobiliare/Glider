555d77e0e9bfb88e9fa5b3bbe8bb4c9cb4b08a34
Daniele Margutti <hello@danielemargutti.com>
2022-06-05 22:40:29 +0200

    Added support for WAL and Vacuum settings on SQLiteDB class

--- Sources/Glider/Transports/SQLite/Support/SQLiteDb+Additions.swift
+++ Sources/Glider/Transports/SQLite/Support/SQLiteDb+Additions.swift
@@ -11,9 +11,21 @@
 //
 
 import Foundation
+import SQLite3
 
 extension SQLiteDb {
     
+    // MARK: WAL
+    
+    /// Set WAL checkpoint mode.
+    ///
+    /// - Parameter mode: mode to set.
+    public func walCheckpoint(mode: CheckpointMode = .passive) throws {
+        try check(sqlite3_wal_checkpoint_v2(handle, nil, mode.flag, nil, nil))
+    }
+    
+    // MARK: - User Schema Version
+
     /// Set the user version of database.
     ///
     /// - Parameters:
@@ -41,6 +53,8 @@ extension SQLiteDb {
         return stmt.integer(column: 0)!
     }
     
+    // MARK: - Foreign Keys
+
     /// Has the database support for foreign keys.
     ///
     /// - Returns: Bool
@@ -62,6 +76,69 @@ extension SQLiteDb {
     public func setForeignKeys(enabled: Bool, schema: String? = nil) throws {
         let sql: String
         sql = schemaStatement(template: "PRAGMA foreign_keys = \(enabled ? "ON" : "OFF")", schema: schema)
+        try exec(sql)
+    }
+    
+    // MARK: - Vacuum
+    
+    /// Set auto vacuum mode.
+    ///
+    /// - Parameters:
+    ///   - autoVacuum: Auto vacuum mode-
+    ///   - schema: Optional schema.
+    public func set(autoVacuum: AutoVacuum, schema: String? = nil) throws {
+        let sql = schemaStatement(template: "PRAGMA %@auto_vacuum = \(autoVacuum.rawValue)", schema: schema)
+        try exec(sql)
+    }
+    
+    /// Get current auto vacuum mode.
+    ///
+    /// - Parameter schema: Optional schema
+    /// - Returns: `AutoVacuum`
+    public func autoVacuum(schema: String? = nil) throws -> AutoVacuum {
+        let sql = schemaStatement(template: "PRAGMA %@auto_vacuum", schema: schema)
+        let stmt = try statement(sql: sql)
+        guard try stmt.step() else {
+            throw DatabaseError(reason: "Error fetching auto vacuum, step failed", code: -1)
+        }
+        return AutoVacuum(rawValue: stmt.integer(column: 0) ?? 0) ?? .none
+    }
+    
+    /// The incremental_vacuum pragma causes up to N pages to be removed from the freelist.
+    /// The database file is truncated by the same amount. The incremental_vacuum pragma has
+    /// no effect if the database is not in auto_vacuum=incremental mode or if there are no pages on the freelist.
+    /// If there are fewer than N pages on the freelist, or if N is less than 1, or if the "(N)" argument is omitted,
+    /// then the entire freelist is cleared.
+    ///
+    /// - Parameters:
+    ///   - pages: Number of pages to remove, 0 or nil will clear the entire free list
+    ///   - schema: Optional schema
+    public func incrementalVacuum(pages: Int? = nil, schema: String? = nil) throws {
+        let sql: String
+        if let pages = pages {
+            sql = schemaStatement(template: "PRAGMA %@incremental_vacuum(\(pages))", schema: schema)
+        } else {
+            sql = schemaStatement(template: "PRAGMA %@incremental_vacuum", schema: schema)
+        }
+        try exec(sql)
+    }
+    
+    /// Manually vacuum the database.
+    ///
+    /// It's important to keep your room neat and tidy! vacuum from time to time to reclaim unused pages,
+    /// caused by deletes, this call vacuums some pages that cannot be reclaimed with auto vacuum.
+    ///
+    /// - Parameters:
+    ///   - schema: Optional schema
+    ///   - into: Optional new database path, if provided, a new vacuumed database will be created in the provided path
+    public func vacuum(schema: String? = nil, into: String? = nil) throws {
+        let sql: String
+        if let into = into {
+            let into_escaped = into.replacingOccurrences(of: "'", with: "''")
+            sql = schemaStatement(template: "VACUUM %@ INTO '\(into_escaped)'", schema: schema)
+        } else {
+            sql = schemaStatement(template: "VACUUM %@", schema: schema)
+        }
         try exec(sql)
     }
     
