60797e057d77223d588e8f9eb06345dccf162641
Daniele Margutti <hello@danielemargutti.com>
2022-06-15 13:57:39 +0200

    Added test for HTTPTransport

--- Tests/GliderTests/Transports/HTTPTransport+Tests.swift
+++ Tests/GliderTests/Transports/HTTPTransport+Tests.swift
@@ -14,12 +14,31 @@ import Foundation
 
 import XCTest
 @testable import Glider
+import Darwin.C
 
-final class HTTPTransportTests: XCTestCase, HTTPTransportDelegate {
+final class HTTPTransportTests: XCTestCase, HTTPTransportDelegate, HTTPServerDelegate {
+    
+    // MARK: - Private Properties
+    
+    private var messagesToSent = 5
+    private var countReceived = 0
+    private var expReceivedMessages: XCTestExpectation?
+    private var expServerStart: XCTestExpectation?
+    private let port = UInt16(8080)
+    private var serverSocket: HTTPServer?
+    
+    // MARK: - Test
     
     func test_HTTPTransportsSending() async throws {
-        let exp = expectation(description: "Test")
+        expServerStart = expectation(description: "Expecting server to start")
+
+        // Start an http server to test data
+        serverSocket = HTTPServer()
+        serverSocket?.delegate = self
+        try serverSocket?.start(port: port)
         
+        expReceivedMessages = expectation(description: "Expecting received messages")
+
         let transport = try HTTPTransport(delegate: self) {
             $0.maxConcurrentRequests = 3
             $0.formatters = [SysLogFormatter()]
@@ -28,25 +47,33 @@ final class HTTPTransportTests: XCTestCase, HTTPTransportDelegate {
             $0.autoFlushInterval = 5
         }
         
+        wait(for: [expServerStart!], timeout: 5)
+
         let log = Log {
             $0.transports = [transport]
             $0.level = .debug
         }
         
-        for i in 0..<5 {
-            log.error?.write("test \(i)")
+        // Send some logs
+        for i in 0..<messagesToSent {
+            log.error?.write("Message \(i)")
         }
         
-        wait(for: [exp], timeout: 80)
+        wait(for: [expReceivedMessages!], timeout: 10)
+        
+        serverSocket?.stop()
     }
+    
+    // MARK: - HTTPTransportDelegate
     
     func httpTransport(_ transport: HTTPTransport,
                        prepareURLRequestsForChunk chunk: AsyncTransport.Chunk) -> [HTTPTransportRequest] {
         
         chunk.map { event, message, attempt in
-            var urlRequest = URLRequest(url: URL(string: "http://www.apple.com")!)
+            var urlRequest = URLRequest(url: URL(string: "http://127.0.0.1:\(port)")!)
             urlRequest.httpBody = message?.asData()
             urlRequest.httpMethod = "POST"
+            urlRequest.timeoutInterval = 10
             
             return HTTPTransportRequest(urlRequest: urlRequest) {
                 $0.maxRetries = 2
@@ -58,14 +85,52 @@ final class HTTPTransportTests: XCTestCase, HTTPTransportDelegate {
     func httpTransport(_ transport: HTTPTransport,
                        didFinishRequest request: HTTPTransportRequest,
                        withResult result: AsyncURLRequestOperation.Response) {
-        
-        let url = request.urlRequest.url!.absoluteString
-        
+                
         switch result {
         case .failure(let error):
-            print("Did complete: \(url): error \(error.localizedDescription)")
+            print("Failed to send data: \(error.localizedDescription)")
         case .success:
-            print("Did complete: \(url): success")
+            print("Data sent successfully!")
+            countReceived += 1
+        }
+        
+        if countReceived == messagesToSent {
+            expReceivedMessages!.fulfill()
+        }
+    }
+    
+    // MARK: - HTTPRequestHandler
+    
+    func serverDidChangeState(_ server: HTTPServer, state: HTTPServer.HTTPServerState) {
+        if state == .running {
+            expServerStart!.fulfill()
+        }
+    }
+    
+    func server(_ server: HTTPServer, didReceiveRequest request: CFHTTPMessage, fileHandle: FileHandle, completion: @escaping () -> Void) {
+        let url = CFHTTPMessageCopyRequestURL(request)!.takeRetainedValue() as URL
+        let method = CFHTTPMessageCopyRequestMethod(request)!.takeRetainedValue() as String
+
+        switch (method, url.path) {
+        case ("POST", "/"):
+            // Echo response
+            let data = CFHTTPMessageCopySerializedMessage(request)!.takeRetainedValue() as Data
+            let response = CFHTTPMessageCreateResponse(kCFAllocatorDefault, 200, nil, kCFHTTPVersion1_1).takeRetainedValue()
+            CFHTTPMessageSetHeaderFieldValue(response, "Content-Type" as CFString, "application/json" as CFString)
+            CFHTTPMessageSetBody(response, data as CFData)
+            assert(CFHTTPMessageIsHeaderComplete(response))
+            // let data = CFHTTPMessageCopySerializedMessage(response)!.takeRetainedValue() as Data
+            fileHandle.write(data)
+            completion()
+        default:
+            // Fallback error
+            let response = CFHTTPMessageCreateResponse(kCFAllocatorDefault, 405, nil, kCFHTTPVersion1_1).takeRetainedValue()
+            CFHTTPMessageSetHeaderFieldValue(response, "Content-Type" as CFString, "text/plain" as CFString)
+            CFHTTPMessageSetBody(response, "405 Method Not Allowed".data(using: .ascii)! as CFData)
+            assert(CFHTTPMessageIsHeaderComplete(response))
+            let data = CFHTTPMessageCopySerializedMessage(response)!.takeRetainedValue() as Data
+            fileHandle.write(data)
+            completion()
         }
     }
     
---
+++ Tests/GliderTests/Transports/Utilities/HTTPServer.swift
@@ -0,0 +1,204 @@
+//
+//  Glider
+//  Fast, Lightweight yet powerful logging system for Swift.
+//
+//  Created by Daniele Margutti
+//  Email: <hello@danielemargutti.com>
+//  Web: <http://www.danielemargutti.com>
+//
+//  Copyright ©2022 Daniele Margutti. All rights reserved.
+//  Licensed under MIT License.
+//
+
+import Foundation
+
+// MARK: - HTTPServer
+
+public class HTTPServer {
+    
+    // MARK: - Public Properties
+    
+    /// State of the server.
+    public private(set) var state = HTTPServerState.idle {
+        didSet { delegate?.serverDidChangeState(self, state: state) }
+    }
+    
+    /// Delegate of the server.
+    public weak var delegate: HTTPServerDelegate?
+    
+    // MARK: - Private Properties
+    
+    private var listeningHandle: FileHandle?
+    private var socket: CFSocket?
+    private var incomingRequests = [FileHandle: CFHTTPMessage]()
+    
+    // MARK: - Public Function
+    
+    /// Start an HTTP server at given port.
+    ///
+    /// - Parameter port: port of listing.
+    public func start(port: UInt16) throws {
+        state = .starting
+        guard let socket = CFSocketCreate(kCFAllocatorDefault, PF_INET, SOCK_STREAM, IPPROTO_TCP, 0, nil, nil) else {
+            throw HTTPServerError.socketCreationFailed
+        }
+        
+        self.socket = socket
+        
+        var reuse = 1
+        let fileDescriptor = CFSocketGetNative(socket)
+        if setsockopt(fileDescriptor, SOL_SOCKET, SO_REUSEADDR, &reuse, socklen_t(MemoryLayout<Int>.size)) != 0 {
+            throw HTTPServerError.socketSetOptionFailed
+        }
+        
+        var noSigPipe = 1
+        if setsockopt(fileDescriptor, SOL_SOCKET, SO_NOSIGPIPE, &noSigPipe, socklen_t(MemoryLayout<Int>.size)) != 0 {
+            throw HTTPServerError.socketSetOptionFailed
+        }
+        
+        var address = sockaddr_in(sin_len: __uint8_t(MemoryLayout<sockaddr_in>.size),
+                                  sin_family: sa_family_t(AF_INET),
+                                  sin_port: port.bigEndian,
+                                  sin_addr: in_addr(s_addr: INADDR_ANY.bigEndian),
+                                  sin_zero: (0, 0, 0, 0, 0, 0, 0, 0))
+        
+        let addressData = Data(bytes: &address, count: MemoryLayout<sockaddr_in>.size)
+        switch CFSocketSetAddress(socket, addressData as CFData) {
+        case .success:
+            break
+        case .error:
+            throw HTTPServerError.socketSetAddressFailed
+        case .timeout:
+            throw HTTPServerError.socketSetAddressTimeout
+        @unknown default:
+            throw HTTPServerError.unknown
+        }
+        
+        if listen(fileDescriptor, 5) != 0 {
+            throw HTTPServerError.socketListenFailed
+        }
+        
+        let listeningHandle = FileHandle(fileDescriptor: fileDescriptor, closeOnDealloc: true)
+        self.listeningHandle = listeningHandle
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(receiveIncomingConnectionNotification(_:)),
+                                               name: .NSFileHandleConnectionAccepted,
+                                               object: listeningHandle)
+        listeningHandle.acceptConnectionInBackgroundAndNotify()
+        state = .running
+    }
+    
+    /// Stop server.
+    public func stop() {
+        state = .stopping
+        NotificationCenter.default.removeObserver(self, name: .NSFileHandleConnectionAccepted, object: nil)
+        listeningHandle?.closeFile()
+        listeningHandle = nil
+        for incomingFileHandle in incomingRequests.keys {
+            stopReceiving(for: incomingFileHandle, close: true)
+        }
+        if let socket = socket {
+            CFSocketInvalidate(socket)
+        }
+        socket = nil
+        state = .idle
+    }
+    
+    // MARK: - Private Functions
+    
+    private func stopReceiving(for incomingFileHandle: FileHandle, close closeFileHandle: Bool) {
+        if closeFileHandle {
+            incomingFileHandle.closeFile()
+        }
+        NotificationCenter.default.removeObserver(self, name: .NSFileHandleDataAvailable, object: incomingFileHandle)
+        incomingRequests.removeValue(forKey: incomingFileHandle)
+    }
+    
+    @objc
+    private func receiveIncomingConnectionNotification(_ notification: Notification) {
+        if let incomingFileHandle = notification.userInfo?[NSFileHandleNotificationFileHandleItem] as? FileHandle {
+            let message = CFHTTPMessageCreateEmpty(kCFAllocatorDefault, true)
+            incomingRequests[incomingFileHandle] = message.autorelease().takeUnretainedValue()
+            NotificationCenter.default.addObserver(self, selector: #selector(receiveIncomingDataNotification(_:)), name: .NSFileHandleDataAvailable, object: incomingFileHandle)
+            incomingFileHandle.waitForDataInBackgroundAndNotify()
+        }
+        listeningHandle?.acceptConnectionInBackgroundAndNotify()
+    }
+    
+    @objc
+    private func receiveIncomingDataNotification(_ notification: Notification) {
+        guard let incomingFileHandle = notification.object as? FileHandle else { return }
+        let data = incomingFileHandle.availableData
+        guard !data.isEmpty else {
+            return stopReceiving(for: incomingFileHandle, close: false)
+        }
+        
+        guard let incomingRequest = incomingRequests[incomingFileHandle] else {
+            return stopReceiving(for: incomingFileHandle, close: true)
+        }
+        
+        guard data.withUnsafeBytes({ bytes in CFHTTPMessageAppendBytes(incomingRequest, bytes, data.count) }) else {
+            return stopReceiving(for: incomingFileHandle, close: true)
+        }
+        
+        guard CFHTTPMessageIsHeaderComplete(incomingRequest) else {
+            return incomingFileHandle.waitForDataInBackgroundAndNotify()
+        }
+        
+        defer {
+            stopReceiving(for: incomingFileHandle, close: false)
+        }
+        
+        delegate?.server(self, didReceiveRequest: incomingRequest, fileHandle: incomingFileHandle) { [weak self] in
+            self?.stopReceiving(for: incomingFileHandle, close: true)
+        }
+    }
+}
+
+extension HTTPServer {
+    
+    // MARK: - HTTPServerState
+    
+    /// State of the server.
+    public enum HTTPServerState {
+        case idle
+        case starting
+        case running
+        case stopping
+    }
+    
+    // MARK: - HTTPServerError
+    
+    /// Error states.
+    public enum HTTPServerError: Error, CustomNSError {
+        case socketCreationFailed
+        case socketSetOptionFailed
+        case socketSetAddressFailed
+        case socketSetAddressTimeout
+        case socketListenFailed
+        case unknown
+        
+        public static let domain: NSErrorDomain = "HTTPServerError"
+    }
+    
+}
+
+// MARK: - HTTPServerDelegate
+
+public protocol HTTPServerDelegate: AnyObject {
+    
+    /// Called when server did change state.
+    ///
+    /// - Parameter server: server.
+    func serverDidChangeState(_ server: HTTPServer, state: HTTPServer.HTTPServerState)
+    
+    /// Called when server receive a new request.
+    ///
+    /// - Parameters:
+    ///   - server: server instance.
+    ///   - request: request receiver.
+    ///   - fileHandle: handler IO to pass response.
+    ///   - completion: completion to call at the end.
+    func server(_ server: HTTPServer, didReceiveRequest request: CFHTTPMessage, fileHandle: FileHandle, completion: @escaping () -> Void)
+
+}
