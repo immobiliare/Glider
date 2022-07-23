cf387b6adba44b4b416354d87dabe121b26ccf0a
Daniele Margutti <hello@danielemargutti.com>
2022-07-23 11:18:43 +0200

    Bump to 0.9.23

--- Glider/Sources/GliderSDK.swift
+++ Glider/Sources/GliderSDK.swift
@@ -20,7 +20,7 @@ public class GliderSDK {
     public static let shared = GliderSDK()
     
     /// SDK Current Version.
-    public static let version = "0.9.22"
+    public static let version = "0.9.23"
 
     /// Identifier of the package
     public static let identifier = "com.glider-logger"
