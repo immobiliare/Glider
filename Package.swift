6b2fa4a905ce513b6dfd98a039bd1264fbe28c60
Daniele Margutti <hello@danielemargutti.com>
2022-08-11 10:08:55 +0200

    Restored sentry-cocoa as package name

--- Package.swift
+++ Package.swift
@@ -46,7 +46,7 @@ let package = Package(
             name: "GliderSentry",
             dependencies: [
                 "Glider",
-                .product(name: "Sentry", package: "Sentry")
+                .product(name: "Sentry", package: "sentry-cocoa")
             ],
             path:"GliderSentry/Sources"
         ),
